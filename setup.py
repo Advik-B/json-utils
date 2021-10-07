99c801ef67b480b936271550d114b7de69f6ec9d
Advik-B <advik.b@gmail.com>
2021-10-07 17:59:47 +0530

    test publish

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Json Utils",                     # This is the name of the package
-    version="1.4.2",                        # The initial release version
+    name="Json_util",                     # This is the name of the package
+    version="1.6.3",                        # The initial release version
     author="Advik Bommu",                     # Full name of the author
-    description="Everything you need for handling json files",
+    description="",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),    # List of all python modules to be installed
@@ -17,7 +17,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=["json-utils"],             # Name of the python package
-    package_dir={'':'.'},     # Directory of the source code of the package
-    install_requires=[]                     # Install other dependencies if any
+    py_modules=["json_util"],             # Name of the python package
+    package_dir={'':'json_util/src'},     # Directory of the source code of the package
+    install_requires=[]                  # Install other dependencies if any
 )
